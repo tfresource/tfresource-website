07b2f025a9a2dfece07681611040082e760e58aa
Billy Charlton <billy@okbecause.com>
2019-01-03 23:34:07 +0100

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  themeConfig: {
+    title: 'Hello',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+        '/',
+        '/Models/'
+    ],
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ docs/Models/README.md
@@ -0,0 +1,15 @@
+# Models
+
+## Four step
+
+Fun stuff.
+
+## MATSim
+
+also fun.
+
+### do we even need third heading
+
+yes.
+:
+
