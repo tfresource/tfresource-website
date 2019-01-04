5db77510f183cb7a6be0e8795ed0e316db5e3117
Billy Charlton <billy@okbecause.com>
2019-01-04 16:45:49 +0100

    build settings

---
+++ .vuepress/scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+npm i -g vuepress
+
+vuepress build
+
+export SITE=`mktemp -d`
+export TIMESTAMP=`date` 
+
+mv .vuepress/dist/* $SITE
+git checkout -b gh-pages
+rm -rf *
+mv $SITE/* .
+
+git add .
+git commit -m "Build: $TIMESTAMP"
+
+git checkout master
+
