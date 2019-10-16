df32909d9bfcdaab9b50ba8134ff6a957ecef4e9
Billy Charlton <billy@okbecause.com>
2019-10-16 10:34:41 +0200

    simplify build.sh

--- .vuepress/scripts/build.sh
+++ .vuepress/scripts/build.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-SOURCE_BRANCH=source
-BUILD_BRANCH=master
-
 yarn install
 yarn build
-
-export SITE=`mktemp -d`
-export TIMESTAMP=`date`
-
-# SURGE:
-# ------
-# surge -d https://tfresource.surge.sh .vuepress/dist
-
-# GITHUB-PAGES:
-# -------------
-#mv .vuepress/dist/* $SITE
-#git checkout -b $BUILD_BRANCH
-#rm -rf *
-#mv $SITE/* .
-#
-#git add .
-#git commit -m "Build: $TIMESTAMP"
-#
-#git checkout $SOURCE_BRANCH
 
