9c4ec79a7041ade5cf5b3c008e84da0ef1ccead8
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-09-07 12:34:17 -0600

    Revise and restructure sidebar
    In summer 2020, the committee determined to implement an outline for the
    website, including putting a table of contents page into the document
    and orienting the website around this new outline. This commit constructs
    a sidebar based on the same topics in the Table of Contents.

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -1,45 +1,79 @@
 module.exports = [
   {
-    title: 'For Contributors',
+    title: 'Intro',
     children: [
-      '/topics/',
-      '/topics/TF_Resource_Volunteers',
-      '/topics/Contributing',
-      '/topics/Tips-for-reviewing-pages',
-      '/topics/Contributing-Dev-Instructions',
+      '/topics/Big_Picture',
+      '/topics/00_Table_Contents',
     ],
   },
   {
-    title: 'Travel Forecasting',
+    title: 'Principles and Concepts',
     children: [
-      '/topics/Big_Picture',
       {
-        title: 'Models and Modeling',
+        title: 'Concepts of Travel Behavior',
         children: [
-          '/topics/Activity_based_models',
-          '/topics/Destination_Choice_Models',
-          '/topics/Freight_modeling',
-          '/topics/Land_use_transport_modeling',
-          '/topics/Mode_choice',
-          '/topics/Network_assignment',
-          '/topics/Public_transport_transit_modeling',
-          '/topics/Sketch_planning_models',
-          '/topics/Spatial_interaction_models',
-          '/topics/Statewide_models',
+          '/topics/Accessibilities/',
+          '/topics/Impedance/',
+          '/topics/Trip_based_models/',
+          '/topics/Activity_based_models/',
+        ],
+      },
+      {
+        title: 'Models of Travel Behavior',
+        children: [
           '/topics/Travel_behavior',
-          '/topics/Trip_based_models',
+          '/topics/Trip_Generation',
+          '/topics/Trip_distribution',
+          '/topics/Mode_choice',
+          '/topics/Destination_Choice_Models',
+          '/topics/Network_assignment',
+          '/topics/Land_use_transport_modeling'
         ],
       },
       {
-        title: 'Forecasting Topics',
+        title: 'Statistics / Econometrics',
         children: [
-          '/topics/Autonomous_vehicles',
-          '/topics/Benefits_of_dynamic_network_models',
-          '/topics/Model_calibration_and_validation',
-          '/topics/New_Starts',
-          '/topics/Project_level_traffic_forecasting',
+          '/topics/Choice_models',
         ],
       },
+    ],
+  },
+  {
+    title: 'Model Development',
+    children: [
+      {
+        title: 'Model Building Blocks',
+        children: [
+          '/topics/Highway_networks',
+          '/topics/Traffic_Analysis_Zone',
+          '/topics/Spatial_data',
+          '/topics/Urban_data',
+          '/topics/Skim_Matrix',
+        ],
+      },
+      {
+        title: 'Model Development Techniques',
+        children: [
+          '/topics/Model_calibration_and_validation',
+        ],
+      },
+      {
+        title: 'Mode-specific Considerations',
+        children: [
+          '/topics/Freight_modeling',
+          '/topics/Public_transport_transit_modeling',
+          '/topics/Autonomous_vehicles',
+          '/topics/Land_use_transport_modeling',
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Model Application and Management',
+    children: [
+      '/topics/New_Starts',
+      '/topics/Project_level_traffic_forecasting',
+      '/topics/Sketch_planning_models',
     ],
   },
   {
@@ -48,6 +82,15 @@ module.exports = [
   },
   {
     title: 'About TFR',
-    children: ['/topics/About', '/topics/Governance', '/topics/Community', '/topics/Content_and_Membership_Council'],
+    children: [
+      '/topics/About',
+      '/topics/Governance',
+      '/topics/Community',
+      '/topics/Content_and_Membership_Council',
+      '/topics/',
+      '/topics/TF_Resource_Volunteers',
+      '/topics/Contributing',
+      '/topics/Contributing-Dev-Instructions',
+    ],
   },
 ]
