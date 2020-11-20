2f934d8346e4f5a98f43b569299047b5cc1a1012
Billy Charlton <billy@okbecause.com>
2020-11-20 16:02:16 +0100

    update build script for recent changes

--- .vuepress/scripts/build.sh
+++ .vuepress/scripts/build.sh
@@ -4,6 +4,7 @@ set -e
 # build list of latest changes
 cd topics
 for each in *; do echo `git log -1 --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
+cd ..
 
 node .vuepress/scripts/acronym-builder.js
 yarn install
