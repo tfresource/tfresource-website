478bedaa7a9f2867747d233d797a7ae807df5bec
Billy Charlton <billy@okbecause.com>
2020-11-20 15:36:39 +0100

    Add new page for /changes:  Recent Changes.

--- .vuepress/scripts/build.sh
+++ .vuepress/scripts/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+# build list of latest changes
+cd topics
+for each in *; do echo `git log -1 --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
+
 node .vuepress/scripts/acronym-builder.js
 yarn install
 yarn build
