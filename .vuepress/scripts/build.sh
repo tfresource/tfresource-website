2dea6f869db7a2ca6f94680532faab0e80483fdc
Billy Charlton <billy@okbecause.com>
2020-11-24 22:21:21 +0100

    get better git history into latest-changes.csv

--- .vuepress/scripts/build.sh
+++ .vuepress/scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 
 # build list of latest changes
 cd topics
-for each in *; do echo `git log -1 --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
+for each in *; do echo `git log -1 --no-merges --first-parent --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
 cd ..
 
 node .vuepress/scripts/acronym-builder.js
