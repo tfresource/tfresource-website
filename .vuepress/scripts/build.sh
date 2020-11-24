a118ed6ffb50fa96499b10f303102eb34a324554
Billy Charlton <billy@okbecause.com>
2020-11-24 23:19:16 +0100

    revert latest-changes build

--- .vuepress/scripts/build.sh
+++ .vuepress/scripts/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
 # build list of latest changes
 cd topics
-for each in *; do echo `git log -1 --no-merges --first-parent --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
+for each in *; do echo `git log -1 --pretty="%as,%aN" $each`,$each; done | sort -rs > ../.vuepress/public/latest-changes.csv
 cd ..
 
 node .vuepress/scripts/acronym-builder.js
